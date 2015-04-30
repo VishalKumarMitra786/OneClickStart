704c6e7f6e149691aabf6b75114803c10390f5dc
vishalkumarmitra786 <vishal.kumarmitra786@gmail.com>
2015-05-01 01:03:37 +0530

    Added OneStartMethod

--- OneClickStart/MainWindow.xaml.cs
+++ OneClickStart/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics;
 
 namespace OneClickStart
 {
@@ -27,7 +29,47 @@ namespace OneClickStart
 
         private void StartBtn_Click(object sender, RoutedEventArgs e)
         {
-         
+            this.OneStartMethod();
+        }
+
+        private void OneStartMethod(){
+            var file = System.IO.Path.GetFullPath("./Config.txt");
+            var isTextFileExist = File.Exists(file);
+            try
+            {
+
+                if (isTextFileExist)
+                {
+                    string[] lines = File.ReadAllLines(file);
+                    foreach (string filePath in lines)
+                    {
+                        //var isFileExist = File.Exists(filePath);
+                        //if (isFileExist)
+                        //{
+                        var tempProcess = new Process();
+                        tempProcess.StartInfo.FileName = filePath;
+                        tempProcess.Start();
+                        //}
+                        //else
+                        //{
+                        //    MessageBox.Show("The Specified Path: " + filePath + " doesn't exist");
+                        //}
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("You have deleted the Config.txt file or renamed it,Make the file with same name or rename the file to Config.txt if renamed.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("The Specified Path  doesn't exist ");
+            }
+            finally
+            {
+                this.Close();
+            }
+            
         }
     }
 }
